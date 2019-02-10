41077d3851a6596e6faafc2ea4550e8e6c97296f
zack <zack.bitcoin@gmail.com>
2019-02-10 19:55:46 -0300

    getting nearer to being able to close the channel.

--- src/js/otc_finisher.js
+++ src/js/otc_finisher.js
@@ -13,12 +13,13 @@
     div.appendChild(start_button);
     //we need a tool for solo-closing the channel, for if your partner refuses to help you. (maybe for now asking for help on a forum is good enough?)
     function start1() {
-        console.log(JSON.stringify(channels_object.channel_manager));
+        //console.log(JSON.stringify(channels_object.channel_manager));
+        var db = {};
         var cd = channels_object.read(their_address.value);
         console.log(JSON.stringify(cd));
         var spk = cd.me;
-        var cid = spk[6];
-        merkle.request_proof("channels", cid, function(c) {
+        db.cid = spk[6];
+        merkle.request_proof("channels", db.cid, function(c) {
             console.log("channel is ");
             console.log(c);
             if (c == "empty") {
@@ -38,20 +39,37 @@
             var key = bet[3];
             console.log(JSON.stringify(key));
             //["market",1,"wqsBDVWpK35TS/VqFYC94QWnNOwClAerYlbtz3AvKtk=",3000,"BHpLwieFVdD5F/z1mdScC9noIZ39HgnwvK8jHqRSBxjzWBssIR1X9LGr8QxTi8fUQws1Q5CGnmTk5dZwzdrGBi4=",10000000,"wqsBDVWpK35TS/VqFYC94QWnNOwClAerYlbtz3AvKtk="]
-            var oracle = key[2];
-            merkle.request_proof("oracles", oracle, function(Or) {
+            var oid = key[2];
+            merkle.request_proof("oracles", oid, function(Or) {
                 console.log(JSON.stringify(Or));
                 //["oracle","wqsBDVWpK35TS/VqFYC94QWnNOwClAerYlbtz3AvKtk=",0,"yAKJm0Zl9jpFBkbolYXdqOKe90nndgCHskmkw8DhSiE=",1000,3,0,"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=","BIVZhs16gtoQ/uUMujl5aSutpImC4va8MewgCveh6MEuDjoDvtQqYZ5FeYcUhY/QLjpCBrXjqvTtFiN4li0Nhjo=",1001,0,0]
                 var result = Or[2];//3 is bad, 2 is false, 1 is true, 0 is still open
-                var done_timer = Or[9];
-                if (result == 0) {//oracle still open
-                    status.innerHTML = ("status: <font color=\"red\"> The bet is not yet settled. The oracle has not been finalized. you need to wait longer to close this oracle. It is expected to be settled a little after block height ").concat((done_timer).toString()).concat("</font>");
-
+                if //(false){//
+                    (result == 0) {//oracle still open
+                    var done_timer = Or[9];
+                    status.innerHTML = ("status: <font color=\"red\"> The bet is not yet settled. The oracle has not been finalized. you need to wait longer to close this channel. It is expected to be settled a little after block height ").concat((done_timer).toString()).concat("</font>");
+                    return 0;
                 };
-                //The light node checks the channel_close_messages on Alice's server. If Bob sent a signed channel_close_tx, and it has the correct final state, then { Carol's light node signs and publishes the tx, it says a message about how the channel is done, it is safe to delete the file from step (6) } otherwise continue
+                return start2(db);
             });
         });
     }
-    // Bob hasn't sent the message to close the channel, so Carol's light node sends the signed tx to Bob as a channel_close_message in Alice's server.
-    //Carol's light node gives Carol a message saying that we are waiting on Bob to close the channel. It gives her a button to start closing the channel without Bob's help, along with a warning about how closing without Bob will break privacy and cost a larger fee, and it will probably take longer than just waiting for Bob to sign the tx. The light node tells her to keep a copy of the file from step (6) until the channel is closed.
+    function start2(db) {
+        return messenger(["read", 1, db.cid, keys.pub()], function(txs) {
+            if (txs == btoa("error")) {
+                //no one has sent us this kind of message yet.
+                //not yet received final state
+                return we_send(db);
+            }
+            //scroll through available txs to see if any are what we want.
+            //if they sent us a channel_close tx, and it is correct, then we sign and publish, then make a message about how the channel is safe to delete.
+            console.log(JSON.stringify(txs));
+        });
+    };
+    function we_send(db) {
+        //generate the channel_close_tx and send it to them.
+        //return messenger(["send", 1, ], function(x) {
+        //make a message about how we signed the tx to close the channel, and now we are just waiting on them to sign it and publish.
+        //give a button to start closing the channel with a channel_solo_close tx, along with a warning about how closing with a solo-close will break privacy and cost a larger fee, and it will probably take longer than just waiting for Bob to sign the tx. The light node tells her to keep a copy of the file from step (6) until the channel is closed.
+    }
 })();
