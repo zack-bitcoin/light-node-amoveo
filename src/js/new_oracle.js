7da035e79c09da86b00d7b70940b2a950823a792
zack <zack.bitcoin@gmail.com>
2019-07-27 05:34:41 -0400

    update the new_oracle.js page to have an interface for making amoveo specific futarchy type oracles.

--- src/js/new_oracle.js
+++ src/js/new_oracle.js
@@ -7,6 +7,9 @@
     var fee = 152050;
     var div = document.createElement("div");
     document.body.appendChild(div);
+    var sb_title = document.createElement("h3");
+    sb_title.innerHTML = "scalar/binary oracle";
+    div.appendChild(sb_title);
     var explanation = document.createElement("p");
     div.append(explanation);
     explanation.innerHTML = "If you want to make a stablecoin, you need to ask for the price of X in VEO. Asking for the price of VEO in X does not work.";
@@ -26,19 +29,55 @@
     glossary.link(div, "binary_oracle");
     div.appendChild(br());
     var scalar = button_maker2("scalar oracle", function(){
-        return new_scalar_oracle(parseInt(starts.value), question.value);
+        return new_scalar_oracle(parseInt(starts.value), question.value, 1, function(){return 0;});
     });
     div.appendChild(scalar);
     glossary.link(div, "scalar_oracle");
-    function new_scalar_oracle(start, question) {
+    div.appendChild(br());
+    var amoveo_futarchy_title = document.createElement("h3");
+    amoveo_futarchy_title.innerHTML = "amoveo governance futarchy oracle";
+    div.appendChild(amoveo_futarchy_title);
+    div.appendChild(br());
+    var futarchy_starts = text_input("futarchy outcome is decided by block: ", div);
+    div.appendChild(br());
+    var futarchy_bets_resolve = text_input("measure impact of futarchy decision on price at block: ", div);
+    div.appendChild(br());
+    var futarchy_price = text_input("the price of VEO in USD half-way between where it would be if we make each of the alternative futarchy decisions:", div);
+    div.appendChild(br());
+    var futarchy_goal = text_input("the futarchy decision being made. example: 'the block reward is more than 0.1 veo'", div);
+    div.appendChild(br());
+    var futarchy_button = button_maker2("amoveo governance futarchy oracle", governance_futarchy_oracle);
+    div.appendChild(futarchy_button);
+    div.appendChild(br());
+    
+    function governance_futarchy_oracle() {
         merkle.request_proof("accounts", keys.pub(), function (acc) {
             var nonce = acc[2]+1;
+            var id = random_cid(32);
+            var start = parseInt(futarchy_bets_resolve.value);
+            var fs = parseInt(futarchy_starts.value);
+            var fg = futarchy_goal.value;
+            var p = Math.floor((1 / parseFloat(futarchy_price.value)) * token_units());
+            var p2 = p*2;
+            //if the block reward on 21 July at noon GMT is above 0.15 return 'bad', else { A = the price of USD in VEO from 0 to 0.3 on 21 July at noon GMT; B = the price of USD in Veo from 0 to 0.3 on 21 August at noon GMT; return ((0.15 - A + B) * 1024 / 0.3)}
+            var common = ", else { A = the price of USD in VEO-satoshis from 0 to ".concat(p2.toString()).concat(" at block ").concat(fs.toString()).concat("; B = the price of USD in VEO-satoshis from 0 to ").concat(p2.toString()).concat(" at block ").concat(start.toString()).concat("; return ((").concat(p.toString()).concat(" - A + B) * 1024 / ").concat(p2.toString()).concat(")}");
+            var question1 = "if ".concat(fg).concat(" return 'bad'").concat(common);
+            var question2 = "if it is not the case that ".concat(fg).concat(" return 'bad'").concat(common);;
+            console.log(start);
+            new_scalar_oracle(start, question1, 1, function(){
+                return new_scalar_oracle(start, question2, 11, function(){return 0;});
+            })
+        });
+    };
+    function new_scalar_oracle(start, question, n, callback) {
+        merkle.request_proof("accounts", keys.pub(), function (acc) {
+            var nonce = acc[2]+n;
             var id = random_cid(32);
             var txs = new_scalar_oracle2(start, question, nonce, id, 10);
 
             return variable_public_get(["txs", [-6].concat(txs)], function(x) {
                 status.innerHTML = "status: <font color=\"green\">successfully attempted to make a scalar oracle with id: ".concat(btoa(id)).concat("</font>");
-                return 0;
+                return callback();
             //var tx = ["oracle_new", keys.pub(), nonce, fee, btoa(question), start, id, 0, 0, 0];
             });
         });
@@ -47,6 +86,7 @@
         if (many == 0) { return 0;}
         var question2 = question.concat(" bit number ").concat((10-many).toString());
         var tx = ["oracle_new", keys.pub(), nonce, fee, btoa(question2), start, btoa(id), 0, 0, 0];
+        console.log(tx);
         var stx = keys.sign(tx);
         return ([stx]).concat(new_scalar_oracle2(start, question, nonce+1, next_oid(id), many - 1));
     };
