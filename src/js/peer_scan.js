b4c5fbc23eaccf625f7185d9fbf77afdfcf2b5df
zack-bitcoin <zack.bitcoin@gmail.com>
2019-12-19 03:05:46 -0300

    update peer_scan to show which forks a node has updated for

--- src/js/peer_scan.js
+++ src/js/peer_scan.js
@@ -1,26 +1,37 @@
 (function(){
+    var view = document.createElement("div");
     var div = document.createElement("div");
     document.body.appendChild(div);
-    var view = document.createElement("div");
+    function list_to_string(x) {
+        if(x.length == 1){ return(x[0].toString());}
+        return (x[0].toString().concat(".").concat(
+            list_to_string(x.slice(1))));
+    };
     function view_peers(p) {
-        if ("[]" == JSON.stringify(p)) { return [];}
-        console.log(JSON.stringify(p[0]));
+        if(p.length == 0){ return([]);}
         var ip = p[0][1][1];
         var port = p[0][1][2];
         var height = p[0][2][1];
-        console.log(ip);
-        console.log(port);
-        console.log(height);
-        return "<p>ip: ".concat(JSON.stringify(ip.slice(1))).concat("height: ").concat(JSON.stringify(height)).concat(", port: ").concat(JSON.stringify(port)).concat("</p>").concat(view_peers(p.slice(1)));
+        
+        var x = getter(["version", 3], url(8080, list_to_string(ip.slice(1))));
+        var_get(x, function(r){
+            var m = "";
+            console.log(JSON.stringify(r))
+            m += "<p>ip: ".concat(JSON.stringify(ip.slice(1))).concat("height: ").concat(JSON.stringify(height)).concat(", port: ").concat(JSON.stringify(port));
+            if(r) {
+                m += (", fork number: ").concat(JSON.stringify(r));
+            };
+            m += "</p>";
+            view.innerHTML += m;
+        });
+        view_peers(p.slice(1));
+        
     };
     function main() {
         variable_public_get(["peers", 2], function(p) {
-            console.log(JSON.stringify(p));
-            var peers_string = view_peers(p.slice(1));
-            view.innerHTML = peers_string;
+            view_peers(p.slice(1));
         });
     };
-    //var button = button_maker2("refresh", function(){ return main()});
     var button = button_maker2("refresh", main);
     div.appendChild(button);
     div.appendChild(view);
