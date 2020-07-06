805e06496b6f03dfc3c48112fdf1a645719bdc21
zack-bitcoin <zack.bitcoin@gmail.com>
2020-07-05 20:09:48 -0400

    add another way of doing http requests, to make it easier to do request from other sites.

--- src/js/rpc.js
+++ src/js/rpc.js
@@ -60,3 +60,36 @@ function messenger(cmd, callback) {
     var foobar = general_get(cmd, 8088);
     var_get(foobar, callback, cmd);
 }
+
+
+function request(cmd, u, callback) {
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange = callback;
+    xmlhttp.open("POST",u,true);
+    xmlhttp.send(JSON.stringify(cmd));
+    return request2(xmlhttp, cmd, u, callback, 100);
+};
+function request2(x, cmd, u, callback, n) {
+    if (n < 1) { return "failed to connect"; }
+    else if (x.status == 400) {
+        //the data we sent to the server got mixed up along the way, so it looks invalid to the server.
+        //So lets re-send the command.
+        setTimeout(function() {
+            return request(cmd, u, callback);
+        }, 200); }
+    else if (x.status == 0) {
+        //this means that the server got our message, and it is still processing a response for us. So lets wait a bit, and then check if it is ready.
+        setTimeout(function() {
+            return request2(x, cmd, u, callback, n - 1);
+        }, 150);
+    }
+    else if (xml_check(x)) {
+        //this means that the server got our message, and it sent a response. The response is ready to read, so lets read it.
+        p = JSON.parse(xml_out(x));
+        return callback(p[1]);
+    }
+    else {
+        //console.log(x.readyState);
+        //console.log(x.status);
+        setTimeout(function() {return request2(x, cmd, u, callback, n);}, 10);}
+};
