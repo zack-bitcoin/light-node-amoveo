7eac0677ae5f3c83852e686d9966a38f3b439ed6
zack-bitcoin <zack.bitcoin@gmail.com>
2020-07-28 09:28:11 -0400

    wrote a rough draft of a library for dealing with the new kind of smart contracts

---
+++ src/js/contracts.js
@@ -0,0 +1,90 @@
+
+
+//Static binary derivative contract based on a single oracle. 
+var static_binary_derivative = atob("AAAAAAF4gxSDFhSDFhSDFAAAAAAghwAAAAABeTpGRw1IFBQAAAAAAYcWFAIAAAADAAAAFoYAAAAAAzpGhACAAAAAFoIAf////xaCiAAAABOIAAAAAAFHFAAAAAABOkaEAP////8WggAAAAAAFoKIRxQAAAAAAjpGhAAAAAAAFoIA/////xaCiEcUFIQAgAAAABaCAH////8WgohISAAAAAAAAAAAA+hI");
+/*
+OID ! 
+macro [ nil ; 
+macro , swap cons ; 
+macro ] swap cons reverse ; 
+macro even_split [ int 2147483648 , int 2147483647 ] ; 
+macro maximum int 4294967295 ; 
+ car drop car swap drop car swap drop car drop 
+int 32 split 
+ OID @ 
+ == if else fail then 
+drop drop int 1 split swap drop binary 3 AAAA swap ++ 
+int 3 == if 
+even_split int 5000 int 1 
+else drop 
+  int 1 == if 
+    [ maximum , int 0 ] 
+    else drop 
+    int 2 == if 
+      [ int 0 , maximum ] 
+      else drop drop 
+      even_split 
+      then 
+    then 
+  int 0 int 1000 
+then 
+*/
+
+function pack_oracle_binary_bet_offer(C) {
+    var code = static_binary_derivative;
+    C.contract_hash = contract_hash_maker(C.oracle_start_height, C.oracle_text);
+    var serialized_offer = 
+        ["pair_buy_offer", C.from, C.nonce,
+         C.start_limit, C.end_limit,
+         C.source_id, C.source_type,
+         C.contract_hash, C.new_id,
+         C.amount1, C.fee1,
+         C.amount2, C.fee2,
+         C.subs1, C.subs2
+        ];
+    var signed_so = keys.sign(serialized_offer);
+    return([signed_so, btoa(C.oracle_text), C.oracle_start_height]);
+};
+
+
+function unpack_oracle_binary_bet_offer(Offer0) {
+    var SO = Offer0[0];
+    var b = verify1(SO);
+    if(!b) {
+        console.log("bad signature on offer");
+        return(0);
+    };
+    var offer = SO[1];
+    var R = {};
+    R.oracle_text = Offer0[1];
+    R.oracle_start_height = Offer0[2];
+    R.from = offer[1];
+    R.nonce = offer[2];
+    R.start_limit = offer[3];
+    R.source_type = offer[4];
+    R.contract_hash = offer[5];
+    var contract_hash = contract_hash_maker(C.oracle_start_height, C.oracle_text);
+    if(!(R.contract_hash == contract_hash)){
+        console.log("bad contract hash");
+        return(0);
+    }
+    R.new_id = offer[6];
+    R.amount1 = offer[7];
+    R.fee1 = offer[8];
+    R.amount2 = offer[9];
+    R.fee2 = offer[10];
+    R.subs1 = offer[11];
+    R.subs2 = offer[12];
+    return(R);
+};
+
+function make_tx(SO) {
+    var fee = 152050;
+    return(["pair_buy_tx", keys.pub(), SO, fee]);
+};
+
+function contract_hash_maker(start_height, oracle_text) {
+    var oracle_id = id_maker(C.oracle_start_height, 0, 0, C.oracle_text);
+    var serialized_oracle_id = string_to_array(atob(oracle_id))
+    var full_code = array_to_string(([2,0,0,0,32]).concat(serialized_oracle_id)).concat(static_binary_erivative);
+    return(hash(string_to_array(full_code)));
--- src/js/otc_finisher.js
+++ src/js/otc_finisher.js
@@ -55,13 +55,13 @@
             });
         });
     };
-    function start1() {
-        //console.log(JSON.stringify(channels_object.channel_manager));
+    function start1(cd, their_address_val) {
         var db = {};
         db.fee = 152050;
-        //var cd = channels_object.read(their_address.value);
-        var their_address_val = Object.keys(channels_object.channel_manager())[0];
-        db.cd = channels_object.read(their_address_val);
+        //var their_address_val = Object.keys(channels_object.channel_manager())[0];
+        //db.cd = channels_object.read(their_address_val);
+        db.cd = cd;
+        db.their_address_val = their_address_val;
         //console.log(JSON.stringify(db.cd));
         //var spk = cd.me;
         db.spk = db.cd.me;
@@ -235,7 +235,8 @@
             console.log(JSON.stringify(tx));
             var stx = keys.sign(tx);
             var imsg = [-6, early_close_code, db.oracle_type_val, result, stx];
-            var their_address_val = Object.keys(channels_object.channel_manager())[0];
+            //var their_address_val = Object.keys(channels_object.channel_manager())[0];
+            var their_address_val = db.their_address_val;
                 //return send_encrypted_message(imsg, their_address_val, function() {
             var balances_string = calc_balances2(db, tx[5], tx[6]);
             status.innerHTML = ("status: <font color=\"blue\">Successfully generated an offer to close the channel.").concat(balances_string).concat(" Tell your partner to visit this page. Do not delete your channel state yet. Click 'get headers' to see if the contract is settled yet. give this data to your partner: </font> ".concat(JSON.stringify(imsg)));
