2bd45ff3a6bb4209f1a14d1d6b9a8bd3c8afea8d
zack-bitcoin <zack.bitcoin@gmail.com>
2020-08-06 08:28:01 -0400

    update to the new version of the binary derivative smart contract

--- src/js/contracts_test_configuration.js
+++ src/js/contracts_test_configuration.js
@@ -1,6 +1,8 @@
 (function(){
     var oracle_height_constant = "5";
-    var cid = "7/ZnSP1bSxHBGwD9MWKhKodvrnElXEJz8ppsum+PQXQ=";
+//    var cid = "7/ZnSP1bSxHBGwD9MWKhKodvrnElXEJz8ppsum+PQXQ=";
+
+    var cid = "Q9z6Ijo4+n6jtXqM13pfebsCSWQvoNDGsbEXvkUiFQQ=";
     
     subcurrency_spender.type(1);
     subcurrency_spender.to("BNEz2wgeaARdPQT049sQOY8Ceox6NjdchBi02F2n4cRGLZg8qbz5lN9d20XpWOWk5pe358+40cmqWSHl9wLna/g=");
@@ -19,7 +21,7 @@ binary_interface_offer.timelimit("2");
     swap_offer.amount1("1000000");
     swap_offer.amount2("1000000");
     swap_offer.cid2(cid);
-    swap_offer.type2("1");
+    swap_offer.type2("2");
     
     resolve_binary_contract.oracle_height(oracle_height_constant);
     resolve_binary_contract.oracle_text("1=1");
--- src/js/explore_swap_offers.js
+++ src/js/explore_swap_offers.js
@@ -18,12 +18,9 @@ var publish_swap_offer = (function() {
     var temp_div = document.createElement("div");
     div.appendChild(temp_div);
     var orders_div = document.createElement("div");
-    var orders_div2 = document.createElement("div");
 
     function refresh(){
         rpc.post(["markets"], function(l) {
-            //console.log("refreshing");
-            //console.log(l);
             l = l.slice(1);
             temp_div.innerHTML = "<h3>available markets</h3>";
             market_buttons(l);
@@ -55,20 +52,17 @@ var publish_swap_offer = (function() {
                      function(z) {
                          var orders = z[1][7];
                          orders = orders.slice(1);
+                         console.log(orders);
                          orders_div.innerHTML = "<h3>available trades in market "
                              .concat(m[2])
                              .concat("</h3>");
                          temp_div.appendChild(orders_div);
                          display_orders(orders);
-                         orders_div.appendChild(orders_div2);
-                         //console.log(JSON.stringify(orders));
                      },
                      s_ip.value, parseInt(s_port.value));
         });
-            //console.log(l[0]);
         temp_div.appendChild(button);
         //["market", nonce, mid, cid1, type1, cid2, type2, 0]
-        //console.log(l[0]);
         return(market_buttons(l.slice(1)));
     };
     function display_orders(l){
@@ -88,19 +82,16 @@ var publish_swap_offer = (function() {
             .concat(". click to see more details.");
         var button = button_maker2(name, function(){
             trade_details(tid);
-//            console.log(JSON.stringify(order[3]));
         });
         orders_div.appendChild(button);
-//        console.log(JSON.stringify(order));
         return(display_orders(l.slice(1)));
     };
     function trade_details(tid){
         rpc.post(["read", 2, tid], function(t){
             t = t[1];
+            console.log(t);
             swap_viewer.offer(JSON.stringify(t));
             swap_viewer.view();
-            //orders_div2.innerHTML = JSON.stringify(t);
-            //console.log(JSON.stringify(t));
         }, s_ip.value, parseInt(s_port.value));
     };
     return({
