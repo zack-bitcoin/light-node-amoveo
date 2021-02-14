91d3c48be2d1d5cf9ce5ca46d8aac7cd27f96052
zack-bitcoin <zack.bitcoin@gmail.com>
2021-02-14 00:08:27 -0300

    change how we calculate the dev fees in the light node to not over-charge certain situations.

--- src/js/multi_tx.js
+++ src/js/multi_tx.js
@@ -1,4 +1,5 @@
 var multi_tx = (function(){
+    var ZERO = btoa(array_to_string(integer_to_array(0, 32)));
     function zero_accounts_nonces(L) {
         for(var i=0; i<L.length; i++){
             //WARNING, this version only works on contract_use_tx. the erlang version works on other types too.
@@ -68,7 +69,7 @@ var multi_tx = (function(){
             //} else if(tx[0] === "contract_simplify_tx"){
             //} else if(tx[0] === "contract_winnings_tx"){
             } else if(tx[0] === "contract_use_tx"){
-                vol += Math.abs(tx[5]);
+                vol += Math.max(0, tx[5]);
             } else if(tx[0] === "market_new_tx"){
                 vol += (tx[8] + tx[9]);
             } else if(tx[0] === "market_liquidity_tx"){
@@ -76,7 +77,15 @@ var multi_tx = (function(){
             } else if(tx[0] === "market_swap_tx"){
                 vol += Math.abs(tx[5]);
             } else if(tx[0] === "swap_tx2"){
-                vol += Math.round(Math.abs((tx[4][1][6] + tx[4][1][9])/2));
+                var offer = tx[4][1];
+                var volx = Math.round(Math.abs((offer[6] + offer[9])/2));
+                if(offer[4] === ZERO){
+                    volx = offer[6];
+                } else if(offer[7] === ZERO){
+                    volx = offer[9];
+                };
+                vol += Math.abs(volx);
+                //Math.round(Math.abs((tx[4][1][6] + tx[4][1][9])/2));
             }
         };
         return(vol);
